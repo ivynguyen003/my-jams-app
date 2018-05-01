5ba8e79a87e436b70465410ad17307f5c0422d57
Ivy Nguyen <ivynguyen003@gmail.com>
2018-05-01 14:22:46 -0700

    add state

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Library extends Component {
+  constructor(props){
+    super(props);
+    this.state = { albums: albumData };
+
+    }
+  }
   render(){
     return(
       <section className="library">
