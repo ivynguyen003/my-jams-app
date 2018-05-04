f429aa58a62d85a21346b9dc46e9c98edcea9820
Ivy Nguyen <ivynguyen003@gmail.com>
2018-05-04 11:13:45 -0700

    link and display album info

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import albumData from './../data/album'
 
 class Album extends Component {
+  constructor(props){
+    super(props);
+    const album = albumData.find( album => {
+      return album.slug === this.props.match.params.slug
+      });
+      this.state = {
+        album: album
+      };
+  }
+
   render() {
     return (
       <section className="album">
-        {this.props.match.params.slug} Album page
+        <section id="album-info">
+          <img id="album-cover-art"/>
+          <div className="album-details">
+            <h1 id="album-title"></h1>
+            <h2 id="artist"></h2>
+            <div id="release-info"></div>
+          </div>
+        </section>
       </section>
     );
   }
