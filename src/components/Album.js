de15ba903d4077af29f2fb946290a400d570e2cc
Ivy Nguyen <ivynguyen003@gmail.com>
2018-05-04 11:44:37 -0700

    add html display song

--- src/components/Album.js
+++ src/components/Album.js
@@ -23,6 +23,15 @@ class Album extends Component {
             <div id="release-info">{this.state.album.releaseInfo}</div>
           </div>
         </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-column"/>
+            <col id="song-number-column"/>
+            <col id="song-number-column"/>
+          </colgroup>   
+          <tbody>
+          </tbody>
+        </table>
       </section>
     );
   }
