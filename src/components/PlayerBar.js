056881a5bc41340128d0c87183f3ec28ad9673e9
Ivy Nguyen <ivynguyen003@gmail.com>
2018-05-04 17:55:45 -0700

    add & link player bar component

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PlayerBar extends Component {
+  render () {
+    return (
+      <section className="play-bar">
+        Player bar goes here
+      </section>
+    )
+  }
+}
+
+export default PlayerBar;
